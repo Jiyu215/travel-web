126c20507f35f47192b80a9f524c5c3bbc851a27
Jiyu215 <102893626+Jiyu215@users.noreply.github.com>
2024-07-17 17:08:23 +0900

    날씨 데이터 추가

--- apiList.js
+++ apiList.js
@@ -1,8 +1,40 @@
-const cities = [
-  '서울', '부산', '인천', '대구', '대전',
-  '광주', '울산', '세종', '강릉', '수원',
-  '청주', '안동', '울릉도', '전주', '포항',
-  '목포', '여수', '제주'
-];
+const cityXYList = {
+  서울 : {x : 96, y: 74},
+  부산 : {x : 98, y: 76},
+  인천 : {x : 55, y: 124},
+  대구 : {x : 89, y: 90},
+  대전 : {x : 67, y: 100},
+  광주 : {x : 65, y: 123},
+  울산 : {x : 102, y: 84},
+  세종 : {x : 65, y: 104},
+  강릉 : {x : 92, y: 131},
+  수원 : {x : 61, y: 120},
+  청주 : {x : 69, y: 107},
+  안동 : {x : 91, y: 106},
+  울릉도 : {x : 127, y: 127},
+  전주 : {x : 63, y: 89},
+  포항 : {x : 102, y: 94},
+  목포 : {x : 50, y: 67},
+  여수 : {x : 73, y: 66},
+  제주 : {x : 55, y: 39},
+}
 
-export {cities};
+const cityKeys = Object.keys(cityXYList);
+const citiesList = document.getElementById("cities-list");
+
+for (let index = 0; index < cityKeys.length; index++) {
+  citiesList.insertAdjacentHTML(
+      "beforeend",
+      `<li value="${cityKeys[index]}">
+      <ul>
+        <li class="image"></li>
+        <li class="text">${cityKeys[index]}</li>
+      </ul>
+      </li>`
+  );
+}
+citiesList.addEventListener("click",(e)=>{
+  
+});
+export {cityXYList,cityKeys};
+
